0487cacec777152e16142e55840a6d9a19f6316b
Robin <156453367+hbgroca@users.noreply.github.com>
2024-09-16 17:02:37 +0200

    Update
    Förändrat upplägget för google play och apple store knapparna.
    Rensat svg filer som inte används.
    Rättat till subscribe e-post fältet så att det markeras när man tabbar.

--- index.js
+++ index.js
@@ -23,19 +23,10 @@ function changeTheme(){
       darkMode = false;
       console.log("changed theme to bright");
       document.getElementById("body").classList = "bright";
-      document.getElementById("toggleSwitch-dot").classList = "toggleSwitch-dot";
-      document.getElementById("manageMoney-link-wrapper-sm").innerHTML = `
-          <img id="appleLink-sm" class="hover" src="svg/linkAppleStore.svg" alt="AppleStore länk">
-          <img id="googleLink-sm" class="hover" src="svg/linkGoogle.svg" alt="Google Store länk">`;
-      
-      document.getElementById("manageMoney-link-wrapper-md").innerHTML = `
-          <img id="appleLink-md" class="hover" src="svg/linkAppleStore-md.svg" alt="AppleStore länk">
-          <img id="googleLink-md" class="hover" src="svg/linkGoogleStore-md.svg" alt="Google Store länk">`;
-                
-      document.getElementById("manageMoney-link-wrapper-lg").innerHTML = `
-          <img id="appleLink-lg" sclass="hover" src="svg/linkAppleStore-lg-bright.svg" alt="AppleStore länk">
-          <img id="googleLink-lg" class="hover" src="svg/linkGoogle-lg-Bright.svg" alt="Google Store länk">`;
-                
+      document.getElementById("toggleSwitch-dot").classList = "toggleSwitch-dot";  
+
+      document.getElementById("appleStore").src = `svg/appstore-light.svg`;
+      document.getElementById("googleStore").src = `svg/googleplay-light.svg`;
     }
     else{
       // Ändrar tema till mörkt
@@ -44,17 +35,8 @@ function changeTheme(){
       document.getElementById("body").classList = "dark";
       document.getElementById("toggleSwitch-dot").classList = "toggleSwitch-dot toggleSwitch-dot-dark";
       
-      document.getElementById("manageMoney-link-wrapper-sm").innerHTML = `
-          <img id="appleLink-sm" class="hover" src="svg/linkAppleStore-Bright.svg" alt="AppleStore länk">
-          <img id="googleLink-sm" class="hover" src="svg/linkGoogle-bright.svg" alt="Google Store länk">`;
-      
-      document.getElementById("manageMoney-link-wrapper-md").innerHTML = `
-          <img id="appleLink-md" class="hover" src="svg/appstore-md-dark.svg" alt="AppleStore länk">
-          <img id="googleLink-md" class="hover" src="svg/googleplay-md-dark.svg" alt="Google Store länk">`;
-                
-      document.getElementById("manageMoney-link-wrapper-lg").innerHTML = `
-          <img id="appleLink-lg" class="hover" src="svg/appstore-lg-dark.svg" alt="AppleStore länk">
-          <img id="googleLink-lg" class="hover" src="svg/googleplay-lg-dark.svg" alt="Google Store länk">`;
+      document.getElementById("appleStore").src = `svg/appstore-dark.svg`;
+      document.getElementById("googleStore").src = `svg/googleplay-dark.svg`;
     }
 }
 
